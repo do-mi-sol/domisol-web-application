534c0701b3117e0f9091eef25c594b68c5d3d821
HeeYeonKim98 <wow9896@naver.com>
2020-09-04 02:13:24 +0900

    test3

--- src/App.js
+++ src/App.js
@@ -7,13 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>에러에러</p>
 
         <p>안녕하세요 희연이예요</p>
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 255936d819a96a16afdc1d43f77ef3f794f0edc1
         <a
           className="App-link"
           href="https://reactjs.org"
