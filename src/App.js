111196aaf4d91a744cf0b5520aaa02b6526d09ad
HeeYeonKim98 <wow9896@naver.com>
2020-09-05 23:34:13 +0900

    temp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>test입니다.</p>
-        <p>test입니다!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          cooperationTest입니다, 도미솔 프로젝트 화이팅!
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>도미솔 프로젝트</div>
+      </div>
+    );
+  }
 }
-
-export default App;
