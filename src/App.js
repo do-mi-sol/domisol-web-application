1194ebe0c84f684fc3e7d223bd13e69d76234698
HeeYeonKim98 <wow9896@naver.com>
2020-09-05 23:21:54 +0900

    retest

--- src/App.js
+++ src/App.js
@@ -7,19 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-<<<<<<< HEAD
-
-=======
-        <p>test입니다.</p>
-        <p>test입니다!</p>
->>>>>>> 3d3df055352d1bf9c6ec18048e1067b42cac52cb
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          cooperationTest입니다, 도미솔 프로젝트 화이팅!
+          cooperationTest입니다, 도미솔 프로젝트 화이팅! 랄랄랄 충돌 개그켬
         </a>
       </header>
     </div>
