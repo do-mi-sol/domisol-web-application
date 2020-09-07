8f919d10279e18d01a50bd4df1566038378c4563
Minjeong-K <minj2058@gmail.com>
2020-09-07 22:58:55 +0900

    signup-id추가

--- src/screens/auth/SignUp.js
+++ src/screens/auth/SignUp.js
@@ -1,68 +1,105 @@
-import React, { Component } from "react";
+import React, {Component} from 'react'
 
 export default class SignUp extends Component {
-  state = {
-    email: "",
-    name: "",
-    password: "",
-    checkpassword: "",
-  };
+    state = {
+        id: '',
+        email: '',
+        name: '',
+        password: '',
+        checkpassword: '',
+    }
 
-  render() {
-    const { email, name, password, checkpassword } = this.state;
+    // python의 range함수와 동일한 역할 (->연령에 사용)
+    range = (start, end) => {
+        var arr = []
+        var length = end - start
+        for (var i = 0; i <= length; i++) {
+            arr[i] = start
+            start++
+        }
+        return arr
+    }
+    //age 만들기 위해 사용
+    make_ageArr = () => {
+        const listItems = this.range(10, 100).map((age) => (
+            <option value={age} key={age.toString()}>
+                {age}
+            </option>
+        ))
+        return listItems
+    }
 
-    return (
-      <form className="">
-        <div>
-          <input type="email" placeholder="EMAIL" onChange={email} />
-          <button>중복확인</button>
-        </div>
+    render() {
+        const {id, email, name, password, checkpassword} = this.state
 
-        <div>
-          <input
-            type="text"
-            className=""
-            placeholder="USER NAME"
-            onChange={name}
-          />
-          <button>중복확인</button>
-        </div>
+        return (
+            <form className="">
+                <div>
+                    <input type="id" placeholder="ID" onChange={id} />
+                    <button>중복확인</button>
+                </div>
 
-        <div>
-          <input
-            type="password"
-            className=""
-            placeholder="PASSWORD"
-            onChange={password}
-          />
-        </div>
+                <div>
+                    <input
+                        type="text"
+                        className=""
+                        placeholder="USER NAME"
+                        onChange={name}
+                    />
+                    <button>중복확인</button>
+                </div>
 
-        <div>
-          <input
-            type="password"
-            className=""
-            placeholder="Confirm PASSWORD"
-            onChange={checkpassword}
-          />
-        </div>
+                <div>
+                    <input
+                        type="password"
+                        className=""
+                        placeholder="PASSWORD"
+                        onChange={password}
+                    />
+                </div>
 
-        <div>
-          <div>
-            <span className="">성별</span>
-            <input type="radio" name="gender" value="남자" />
-            남성
-            <input type="radio" name="gender" value="여자" />
-            여성
-          </div>
+                <div>
+                    <input
+                        type="password"
+                        className=""
+                        placeholder="Confirm PASSWORD"
+                        onChange={checkpassword}
+                    />
+                </div>
+                <div>
+                    <input
+                        type="email"
+                        placeholder="EMAIL"
+                        onChange={email}
+                    />
+                </div>
+                <div>
+                    <div>
+                        <span className="">성별</span>
+                        <input
+                            type="radio"
+                            name="gender"
+                            value="남자"
+                        />
+                        남성
+                        <input
+                            type="radio"
+                            name="gender"
+                            value="여자"
+                        />
+                        여성
+                    </div>
 
-          <div className="">
-            <span className="">연령</span>
-            <select className=""></select>
-          </div>
-        </div>
+                    <div className="">
+                        <span className="">연령</span>
+                        <select className="">
+                            <this.make_ageArr />
+                        </select>
+                    </div>
+                </div>
 
-        <button className="">Sign Up</button>
-      </form>
-    );
-  }
+                <button className="">Sign Up</button>
+            </form>
+        )
+    }
 }
