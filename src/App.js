bcaebf1741c961a8668e6550da21eaef68c28fbc
Minjeong-K <minj2058@gmail.com>
2020-09-07 21:25:15 +0900

    app에 Signup 임시 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
+import SignUp from './screens/auth/SignUp';
 
 export default class App extends Component {
-  render() {
+  render () {
     return (
       <div>
         <div>도미솔 프로젝트 화이팅! 제발 마지막이 되길..</div>
         <div>한정</div>
         <div>한정</div>
+        {/* SignUp 확인 */}
+        <SignUp />
       </div>
     );
   }
