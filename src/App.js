a68eec2f6daf76ee4f5ea64b4595a8bc70490c4c
HeeYeonKim98 <wow9896@naver.com>
2020-09-07 14:28:08 +0900

    update app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <div>도미솔 프로젝트 화이팅! 제발 마지막이 되길..</div>
+        <div>도미솔 프로젝트 화이팅! 제발 마지막이 되길.ㅓㅗㅓㅗㅇㄹㅓ</div>
       </div>
     );
   }
