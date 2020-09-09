74d748f9648e994f6710e64224cc3ce8b2fb0c6f
Minjeong-K <minj2058@gmail.com>
2020-09-10 03:41:49 +0900

    app.js 주석처리...

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, {Component} from 'react';
-import SignUp from './screens/auth/SignUp';
+import React, {Component} from 'react'
+import SignUp from './screens/auth/SignUp'
 
 export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <div>도미솔 프로젝트 화이팅! 제발 마지막이 되길..</div>
-        <div>한정</div>
-        <div>한정</div>
-        {/* SignUp 확인 */}
-        <SignUp />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div>
+                    도미솔 프로젝트 화이팅! 제발 마지막이 되길..
+                </div>
+                <div>한정</div>
+                <div>한정</div>
+                {/* SignUp 확인 */}
+                {/* <SignUp /> */}
+            </div>
+        )
+    }
 }
