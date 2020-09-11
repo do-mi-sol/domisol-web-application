838b8db17363075bbd910e34f9cba8b45787261e
HeeYeonKim98 <wow9896@naver.com>
2020-09-12 02:14:25 +0900

    repackage-1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, {Component} from 'react'
-import SignUp from './screens/auth/SignUp'
+import React, { Component } from "react";
+// import SignUp from "./screens/auth/SignUp";
 
 export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <div>
-                    도미솔 프로젝트 화이팅! 제발 마지막이 되길..
-                </div>
-                <div>한정</div>
-                <div>한정</div>
-                <div>혜림</div>
-                {/* SignUp 확인 */}
-                {/* <SignUp /> */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div>도미솔 프로젝트 화이팅! 제발 마지막이 되길..</div>
+        <div>한정</div>
+        <div>한정</div>
+        <div>혜d림d</div>
+        {/* SignUp 확인 */}
+        {/* <SignUp /> */}
+      </div>
+    );
+  }
 }
