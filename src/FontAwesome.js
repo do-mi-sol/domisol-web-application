11c4a396845c6eae947cf608ddfb1e9ac5fae228
HeeYeonKim98 <wow9896@naver.com>
2020-09-14 15:38:05 +0900

    update ex

--- src/FontAwesome.js
+++ src/FontAwesome.js
@@ -1,5 +1,5 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faSmile, faAd, faSms } from "@fortawesome/free-solid-svg-icons";
+import { faSmile, faAd, faSms, faPoo } from "@fortawesome/free-solid-svg-icons";
 
-library.add(fab, faSmile, faAd, faSms);
+library.add(fab, faSmile, faAd, faSms, faPoo);
--- src/screens/main/Board.js
+++ src/screens/main/Board.js
@@ -1,116 +1,118 @@
-import React, {Component} from 'react';
-import BestBoard from '../auth/components/BestBoard';
-import MainBoard from '../auth/components/MainBoard';
-import TodayReplyBoard from '../auth/components/TodayReplyBoard';
-import TodayBoard from '../auth/components/TodayBoard';
-import Pagination from '../auth/components/Pagination';
-import 'bootstrap/dist/css/bootstrap.css';
-import {Button, Container, Row, Col} from 'react-bootstrap';
-import _ from 'lodash';
-import '../css/Board.css';
-import {paginate} from '../auth/components/paginate';
+import React, { Component } from "react";
+import BestBoard from "../auth/components/BestBoard";
+import MainBoard from "../auth/components/MainBoard";
+import TodayReplyBoard from "../auth/components/TodayReplyBoard";
+import TodayBoard from "../auth/components/TodayBoard";
+import Pagination from "../auth/components/Pagination";
+import "bootstrap/dist/css/bootstrap.css";
+import { Button, Container, Row, Col } from "react-bootstrap";
+import _ from "lodash";
+import "../css/Board.css";
+import { paginate } from "../auth/components/paginate";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default class Board extends React.Component {
   state = {
     /*******************************데이터 받아오면 없어짐****************************************/
     bestboards: [
       {
-        title: '글제목1',
+        title: "글제목1",
       },
       {
-        title: '글제목2',
+        title: "글제목2",
       },
       {
-        title: '글제목3',
+        title: "글제목3",
       },
     ],
     boards: [
       {
-        gender: '성별',
-        id: 'id',
-        title: '글제목1',
-        time: '시간',
-        likes: '좋아요',
-        hits: '조회수',
-        comments: '댓글수',
+        gender: "성별",
+        id: "id",
+        title: "글제목1",
+        time: "시간",
+        likes: "좋아요",
+        hits: "조회수",
+        comments: "댓글수",
       },
       {
-        gender: 'woman',
-        id: '최지혜',
-        title: '글제목1',
-        time: '14:10',
+        gender: "woman",
+        id: "최지혜",
+        title: "글제목1",
+        time: "14:10",
         likes: 2,
         hits: 100,
         comments: 3,
       },
       {
-        gender: 'woman',
-        id: '최지혜',
-        title: '글제목1',
-        time: '14:10',
+        gender: "woman",
+        id: "최지혜",
+        title: "글제목1",
+        time: "14:10",
         likes: 2,
         hits: 100,
         comments: 3,
       },
       {
-        gender: 'woman',
-        id: '최지혜',
-        title: '글제목1',
-        time: '14:10',
+        gender: "woman",
+        id: "최지혜",
+        title: "글제목1",
+        time: "14:10",
         likes: 2,
         hits: 100,
         comments: 3,
       },
       {
-        gender: 'woman',
-        id: '최지혜',
-        title: '글제목1',
-        time: '14:10',
+        gender: "woman",
+        id: "최지혜",
+        title: "글제목1",
+        time: "14:10",
         likes: 2,
         hits: 100,
         comments: 3,
       },
       {
-        gender: 'woman',
-        id: '최지혜',
-        title: '글제목1',
-        time: '14:10',
+        gender: "woman",
+        id: "최지혜",
+        title: "글제목1",
+        time: "14:10",
         likes: 2,
         hits: 100,
         comments: 3,
       },
       {
-        gender: 'woman',
-        id: '최지혜',
-        title: '글제목1',
-        time: '14:10',
+        gender: "woman",
+        id: "최지혜",
+        title: "글제목1",
+        time: "14:10",
         likes: 2,
         hits: 100,
         comments: 3,
       },
       {
-        gender: 'woman',
-        id: '최지혜',
-        title: '글제목1',
-        time: '14:10',
+        gender: "woman",
+        id: "최지혜",
+        title: "글제목1",
+        time: "14:10",
         likes: 2,
         hits: 100,
         comments: 3,
       },
       {
-        gender: 'woman',
-        id: '최지혜',
-        title: '글제목1',
-        time: '14:10',
+        gender: "woman",
+        id: "최지혜",
+        title: "글제목1",
+        time: "14:10",
         likes: 2,
         hits: 100,
         comments: 3,
       },
       {
-        gender: 'man',
-        id: '강민정',
-        title: '글제목2',
-        time: '05:10',
+        gender: "man",
+        id: "강민정",
+        title: "글제목2",
+        time: "05:10",
         likes: 5,
         hits: 1000,
         comments: 30,
@@ -118,46 +120,46 @@ export default class Board extends React.Component {
     ],
     todayboards: [
       {
-        title: '글제목1',
-        community: '뫄뫄게시판',
+        title: "글제목1",
+        community: "뫄뫄게시판",
       },
       {
-        title: '글제목2',
-        community: '뫄뫄게시판',
+        title: "글제목2",
+        community: "뫄뫄게시판",
       },
       {
-        title: '글제목3',
-        community: '뫄뫄게시판',
+        title: "글제목3",
+        community: "뫄뫄게시판",
       },
       {
-        title: '글제목4',
-        community: '뫄뫄게시판',
+        title: "글제목4",
+        community: "뫄뫄게시판",
       },
       {
-        title: '글제목5',
-        community: '뫄뫄게시판',
+        title: "글제목5",
+        community: "뫄뫄게시판",
       },
     ],
     replyboards: [
       {
-        reply: '댓글1',
-        community: '뫄뫄게시판',
+        reply: "댓글1",
+        community: "뫄뫄게시판",
       },
       {
-        reply: '댓글2',
-        community: '뫄뫄게시판',
+        reply: "댓글2",
+        community: "뫄뫄게시판",
       },
       {
-        reply: '댓글3',
-        community: '뫄뫄게시판',
+        reply: "댓글3",
+        community: "뫄뫄게시판",
       },
       {
-        reply: '댓글4',
-        community: '뫄뫄게시판',
+        reply: "댓글4",
+        community: "뫄뫄게시판",
       },
       {
-        reply: '댓글5',
-        community: '뫄뫄게시판',
+        reply: "댓글5",
+        community: "뫄뫄게시판",
       },
     ],
     pagesize: 3, //한페이지에 보여줄 글 개수
@@ -165,15 +167,15 @@ export default class Board extends React.Component {
     currentPage: 1, //현재 페이지
     /*******************************데이터 받아오면 없어짐****************************************/
   };
-  handlePageChange = page => {
-    this.setState ({currentPage: page});
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
   };
-  render () {
+  render() {
     const replyboards = this.state.replyboards;
     const todayboards = this.state.todayboards;
     const bestboards = this.state.bestboards;
     const boards = this.state.boards;
-    const {length: count} = this.state.boards;
+    const { length: count } = this.state.boards;
     const pageSize = this.state.pagesize;
     const itemsCount = this.state.itemsCount;
     const currentPage = this.state.currentPage;
@@ -184,13 +186,14 @@ export default class Board extends React.Component {
         <div>잘하고싶다..연애 게시판</div>
         <br />
         <div>실시간 베스트</div>
+        <FontAwesomeIcon className="icon" icon="poo" />
 
         <table borders="1">
           <tbody>
             <ol class="list-group list-group-flush">
-              {bestboards.map (row => (
+              {bestboards.map((row) => (
                 <li class="list-group-item">
-                  {' '}
+                  {" "}
                   <BestBoard title={row.title} />
                 </li>
               ))}
@@ -201,7 +204,7 @@ export default class Board extends React.Component {
         {/*메인게시판*/}
         <table borders="1">
           <tbody>
-            {boards.map (row => (
+            {boards.map((row) => (
               <MainBoard
                 gender={row.gender}
                 title={row.title}
@@ -227,7 +230,7 @@ export default class Board extends React.Component {
         <table borders="1">
           <tbody>
             <ol>
-              {todayboards.map (row => (
+              {todayboards.map((row) => (
                 <li>
                   <TodayBoard title={row.title} community={row.community} />
                 </li>
@@ -246,7 +249,7 @@ export default class Board extends React.Component {
         <table borders="1">
           <tbody>
             <ol>
-              {replyboards.map (row => {
+              {replyboards.map((row) => {
                 return (
                   <li>
                     <TodayReplyBoard
