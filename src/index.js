74d962a88da0840be14b3d2d3f994b902473f4af
HeeYeonKim98 <wow9896@naver.com>
2020-09-21 14:45:05 +0900

    update module

--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {createBrowserHistory} from 'history'
-import {Router, Route, Switch} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
 
-import Header from '../src/components/header/Header'
-import Login from './screens/auth/Login'
-import FindID from './screens/auth/FindID'
-import FindPW from './screens/auth/FindPW'
-import SignUp from './screens/auth/SignUp'
-import Home from './screens/main/Home'
-import Board from './screens/main/Board'
+import Header from "../src/components/header/Header";
+import Login from "./screens/auth/Login";
+import FindID from "./screens/auth/FindID";
+import FindPW from "./screens/auth/FindPW";
+import SignUp from "./screens/auth/SignUp";
+import Home from "./screens/main/Home";
+import Board from "./screens/main/Board";
+import Detail from "./screens/main/Detail";
+import Write from "./screens/main/Write";
 
-import './FontAwesome'
+import "./FontAwesome";
 
-var hist = createBrowserHistory()
+var hist = createBrowserHistory();
 
 ReactDOM.render(
-    <Router history={hist}>
-        <Header />
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/board" component={Board} />
-            <Route exact path="/findid" component={FindID} />
-            <Route exact path="/findpw" component={FindPW} />
-        </Switch>
-    </Router>,
-    document.getElementById('root')
-)
+  <Router history={hist}>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/board" component={Board} />
+      <Route exact path="/findid" component={FindID} />
+      <Route exact path="/findpw" component={FindPW} />
+      <Route exact path="/detail" component={Detail} />
+      <Route exact path="/write" component={Write} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
--- src/screens/main/Write.js
+++ src/screens/main/Write.js
@@ -1,118 +1,125 @@
-import React, {Component} from 'react'
-import '../../assets/css/Write.css'
-import {Grid, Box} from '@material-ui/core'
-import DMSButton from '../../components/customs/DMSButton'
-import {FormGroup, Label, Input} from 'reactstrap'
+import React, { Component } from "react";
+import "../../assets/css/Write.css";
+import { Grid, Box } from "@material-ui/core";
+import DMSButton from "../../components/customs/DMSButton";
+import { FormGroup, Label, Input } from "reactstrap";
 
 export default class Write extends Component {
-    state = {
-        test: 'https://cdn.clien.net/web/api/file/F01/3802215/92348cd7fe8441e8956.JPG',
-        file: '',
-        previewURL: '',
-    }
-    //글쓰기 버튼 클릭함수
-    clickWrite = () => {
-        console.log(this.state)
-    }
+  state = {
+    test:
+      "https://cdn.clien.net/web/api/file/F01/3802215/92348cd7fe8441e8956.JPG",
+    file: "",
+    previewURL: "",
+  };
+  //글쓰기 버튼 클릭함수
+  clickWrite = () => {
+    console.log(this.state);
+  };
 
-    //title,contents저장함수
-    handleChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value,
-        })
-    }
+  //title,contents저장함수
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    //사진저장후 프리뷰함수
-    handleFileOnChange = (event) => {
-        event.preventDefault()
-        let reader = new FileReader()
-        let file = event.target.files[0]
-        reader.onloadend = () => {
-            this.setState({
-                test: '',
-                file: file,
-                previewURL: reader.result,
-            })
-        }
-        reader.readAsDataURL(file)
-    }
+  //사진저장후 프리뷰함수
+  handleFileOnChange = (event) => {
+    event.preventDefault();
+    let reader = new FileReader();
+    let file = event.target.files[0];
+    reader.onloadend = () => {
+      this.setState({
+        test: "",
+        file: file,
+        previewURL: reader.result,
+      });
+    };
+    reader.readAsDataURL(file);
+  };
 
-    handleSudmit = (e) => {
-        e.preventDefult()
-        this.props.onSaveData(this.state)
-        this.setState({})
-    }
+  handleSudmit = (e) => {
+    e.preventDefult();
+    this.props.onSaveData(this.state);
+    this.setState({});
+  };
 
-    render() {
-        //이미지크기조절,이미지여러개 받기,이미지프리뷰슬라이드처리,css,사용자 정보 받기,로고
-        let talk_preview = null
-        if (this.state.file !== '') {
-            talk_preview = <img className="talk_preview" src={this.state.previewURL} alt="" />
-        }
-        return (
-            <div className="container">
-                <form onSubmit={this.handleSudmit}>
-                    <Grid container spacing={1} style={{marginTop: 30, marginBottom: 20}}>
-                        <Grid item xs={12} sm={6}>
-                            <Box p={2}>
-                                <Label for="talk_img">
-                                    <img className="talk-img" src={this.state.test} alt="" />
-                                </Label>
-                                <Input
-                                    type="file"
-                                    id="talk_img"
-                                    accept="image/jpg,image/png,image/jpeg"
-                                    name="talk_img"
-                                    className="talk-input"
-                                    onChange={this.handleFileOnChange}
-                                />{' '}
-                                {talk_preview}
-                            </Box>
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <Box p={2}>
-                                <h6>잘하고싶다..연애</h6>
-                                <hr className="yellow-line" />
-                                <FormGroup>
-                                    <Label className="title-label" for="exampleText">
-                                        제목:{' '}
-                                    </Label>
-                                    <Input
-                                        type="textarea"
-                                        name="text"
-                                        id="exampleText"
-                                        className="title-input"
-                                        placeholder="제목을 입력해주세요"
-                                        label="제목:"
-                                        inputProps={{'aria-label': 'description'}}
-                                        onChange={this.handleChange}
-                                    />
-                                    <Input
-                                        type="textarea"
-                                        name="text"
-                                        id="exampleText"
-                                        className="contents-input"
-                                        //placeholder="내용을 입력해주세요"
-                                        onChange={this.handleChange}
-                                    />
-                                </FormGroup>
-                            </Box>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Box className="write-button-box" p={2}>
-                                {' '}
-                                <DMSButton
-                                    className="write-button"
-                                    variant="contained"
-                                    onClick={this.clickWrite}
-                                >
-                                    글쓰기
-                                </DMSButton>
-                            </Box>
-                        </Grid>
-                    </Grid>
-                </form>
-            </div>
-        )
+  render() {
+    //이미지크기조절,이미지여러개 받기,이미지프리뷰슬라이드처리,css,사용자 정보 받기,로고
+    let talk_preview = null;
+    if (this.state.file !== "") {
+      talk_preview = (
+        <img className="talk_preview" src={this.state.previewURL} alt="" />
+      );
     }
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSudmit}>
+          <Grid
+            container
+            spacing={1}
+            style={{ marginTop: 30, marginBottom: 20 }}
+          >
+            <Grid item xs={12} sm={6}>
+              <Box p={2}>
+                <Label for="talk_img">
+                  <img className="talk-img" src={this.state.test} alt="" />
+                </Label>
+                <Input
+                  type="file"
+                  id="talk_img"
+                  accept="image/jpg,image/png,image/jpeg"
+                  name="talk_img"
+                  className="talk-input"
+                  onChange={this.handleFileOnChange}
+                />{" "}
+                {talk_preview}
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Box p={2}>
+                <h6>잘하고싶다..연애</h6>
+                <hr className="yellow-line" />
+                <FormGroup>
+                  <Label className="title-label" for="exampleText">
+                    제목:{" "}
+                  </Label>
+                  <Input
+                    type="textarea"
+                    name="text"
+                    id="exampleText"
+                    className="title-input"
+                    placeholder="제목을 입력해주세요"
+                    label="제목:"
+                    inputProps={{ "aria-label": "description" }}
+                    onChange={this.handleChange}
+                  />
+                  <Input
+                    type="textarea"
+                    name="text"
+                    id="exampleText"
+                    className="contents-input"
+                    //placeholder="내용을 입력해주세요"
+                    onChange={this.handleChange}
+                  />
+                </FormGroup>
+              </Box>
+            </Grid>
+            <Grid item xs={12}>
+              <Box className="write-button-box" p={2}>
+                {" "}
+                <DMSButton
+                  className="write-button"
+                  variant="contained"
+                  onClick={this.clickWrite}
+                >
+                  글쓰기
+                </DMSButton>
+              </Box>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    );
+  }
 }
