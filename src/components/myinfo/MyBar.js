6ed74e55e1110237bfe3c36ddfc7f781300aca00
HeeYeonKim98 <wow9896@naver.com>
2020-10-19 21:11:48 +0900

    myinfo view 수정 전 저장

--- src/components/myinfo/MyBar.js
+++ src/components/myinfo/MyBar.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { Component } from "react";
 import { Grid, Divider, Avatar, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+
+import axios from "axios";
+
+import URL from "../../NET";
 
 const useStyles = makeStyles((theme) => ({
     barContainer: {
@@ -21,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
     bar: {
         width: "80%",
         minWidth: 800,
-        backgroundColor: "#ffe566",
+        backgroundColor: "#d6d6d4",
         margin: theme.spacing(2),
         borderRadius: 20,
         boxShadow: "12px 12px 12px #e6e6e6",
@@ -35,118 +39,162 @@ const useStyles = makeStyles((theme) => ({
         right: "25%",
     },
 }));
-const MyBar = ({ profile, name, myLevel, myWritten, like, myReply }) => {
-    const classes = useStyles();
 
-    return (
-        <div className={classes.barContainer}>
-            <Grid
-                container
-                direction="row"
-                spacing={5}
-                justify="space-around"
-                alignItems="center"
-                className={classes.bar}
-            >
-                {/* 이미지 */}
-                <Grid item>
-                    <Avatar
-                        src={profile}
-                        className={classes.large}
-                        style={{ width: 130, height: 130 }}
-                    />
-                </Grid>
+export default class MyBar extends Component {
+    state = {
+        profile: "/broken-image.jpg",
+        name: "",
+        myLevel: 0,
+        myWritten: 0,
+        like: 0,
+        myReply: 0,
+    };
+    requestInfo = async () => {
+        const token = await localStorage.getItem("token");
+        const bearer = `Bearer ${token}`;
 
-                {/* 아이디 */}
-                <Grid item>
-                    <Typography variant="h4" color="textPrimary">
-                        {name} 님
-                    </Typography>
-                </Grid>
+        if (token == null) {
+            this.setState({
+                id: "비회원",
+                email: "비회원",
+                name: "비회원",
+            });
+            window.location.assign("/login");
+            alert("로그인이 필요한 페이지입니다. 로그인 후 이용해주세요.");
+        } else {
+            await axios
+                .post(URL.account, null, {
+                    headers: {
+                        Authorization: bearer,
+                    },
+                })
+                .then((res) => res.data)
+                .then((body) => {
+                    console.log(body);
 
-                <Divider orientation="vertical" variant="middle" flexItem />
+                    if (body.success) {
+                        const { name } = body.data.user;
+                        this.setState({
+                            name,
+                        });
+                    } else {
+                        alert(body.message);
+                    }
+                });
+        }
+    };
 
-                <Grid item xs={5}>
-                    <Grid
-                        container
-                        spacing={1}
-                        justify="space-around"
-                        alignItems="center"
-                        direction="row"
-                        className={classes.innerGrid}
-                    >
-                        <Grid item>
-                            <Grid
-                                container
-                                justify="center"
-                                alignItems="center"
-                                direction="column"
-                                style={{ height: "100%" }}
-                            >
-                                <Grid item>
-                                    <Typography
-                                        variant="subtitle1"
-                                        align="center"
-                                        // key={line}
-                                    >
-                                        나의 레벨
-                                    </Typography>
-                                    <Typography variant="h4" color="textPrimary" align="center">
-                                        {myLevel}
-                                    </Typography>
-                                </Grid>
-                                <Grid item>
-                                    <Typography
-                                        variant="subtitle1"
-                                        align="center"
-                                        // key={line}
-                                    >
-                                        좋아요
-                                    </Typography>
-                                    <Typography variant="h4" color="textPrimary" align="center">
-                                        {like}
-                                    </Typography>
+    componentDidMount() {
+        this.requestInfo();
+    }
+    render() {
+        const { profile, name, myLevel, myReply, myWritten, like } = this.state;
+        return (
+            <div className={useStyles.barContainer}>
+                <Grid
+                    container
+                    direction="row"
+                    spacing={5}
+                    justify="space-around"
+                    alignItems="center"
+                    className={useStyles.bar}
+                >
+                    <Grid item>
+                        <Avatar
+                            src={profile}
+                            className={useStyles.large}
+                            style={{ width: 130, height: 130 }}
+                        />
+                    </Grid>
+
+                    <Grid item>
+                        <Typography variant="h4" color="textPrimary">
+                            {name} 님
+                        </Typography>
+                    </Grid>
+
+                    <Divider orientation="vertical" variant="middle" flexItem />
+
+                    <Grid item xs={5}>
+                        <Grid
+                            container
+                            spacing={1}
+                            justify="space-around"
+                            alignItems="center"
+                            direction="row"
+                            className={useStyles.innerGrid}
+                        >
+                            <Grid item>
+                                <Grid
+                                    container
+                                    justify="center"
+                                    alignItems="center"
+                                    direction="column"
+                                    style={{ height: "100%" }}
+                                >
+                                    <Grid item>
+                                        <Typography
+                                            variant="subtitle1"
+                                            align="center"
+                                            // key={line}
+                                        >
+                                            나의 레벨
+                                        </Typography>
+                                        <Typography variant="h4" color="textPrimary" align="center">
+                                            {myLevel}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography
+                                            variant="subtitle1"
+                                            align="center"
+                                            // key={line}
+                                        >
+                                            좋아요
+                                        </Typography>
+                                        <Typography variant="h4" color="textPrimary" align="center">
+                                            {like}
+                                        </Typography>
+                                    </Grid>
                                 </Grid>
                             </Grid>
-                        </Grid>
-                        <Grid item>
-                            <Grid
-                                container
-                                justify="space-between"
-                                alignItems="center"
-                                direction="column"
-                            >
-                                <Grid item>
-                                    <Typography
-                                        variant="subtitle1"
-                                        align="center"
-                                        // key={line}
-                                    >
-                                        내가 쓴 글
-                                    </Typography>
-                                    <Typography variant="h4" color="textPrimary" align="center">
-                                        {myWritten}
-                                    </Typography>
-                                </Grid>
-                                <Grid item>
-                                    <Typography
-                                        variant="subtitle1"
-                                        align="center"
-                                        // key={line}
-                                    >
-                                        나의 답변
-                                    </Typography>
-                                    <Typography variant="h4" color="textPrimary" align="center">
-                                        {myReply}
-                                    </Typography>
+                            <Grid item>
+                                <Grid
+                                    container
+                                    justify="space-between"
+                                    alignItems="center"
+                                    direction="column"
+                                >
+                                    <Grid item>
+                                        <Typography
+                                            variant="subtitle1"
+                                            align="center"
+                                            // key={line}
+                                        >
+                                            내가 쓴 글
+                                        </Typography>
+                                        <Typography variant="h4" color="textPrimary" align="center">
+                                            {myWritten}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography
+                                            variant="subtitle1"
+                                            align="center"
+                                            // key={line}
+                                        >
+                                            나의 답변
+                                        </Typography>
+                                        <Typography variant="h4" color="textPrimary" align="center">
+                                            {myReply}
+                                        </Typography>
+                                    </Grid>
                                 </Grid>
                             </Grid>
                         </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
-        </div>
-    );
-};
-
-export default MyBar;
+            </div>
+        );
+    }
+}
--- src/components/myinfo/MyProfile.js
+++ src/components/myinfo/MyProfile.js
@@ -1,109 +1,165 @@
-import React from "react";
+import React, { Component } from "react";
 import { Form, FormGroup, Label, Input, Col } from "reactstrap";
 import { Avatar, Paper } from "@material-ui/core";
+import axios from "axios";
+
+import URL from "../../NET";
+
 import "../../assets/css/MyProfile.css";
 
-const MyProfile = (props) => {
-    return (
-        <div>
-            <h3 style={{ color: "gray", textShadow: "1px 1px 1px gray", marginBottom: 20 }}>
-                나의 정보
-            </h3>
-            <Form className="profile-form">
-                <Paper
-                    elevation={3}
-                    style={{
-                        width: "100%",
-                        minWidth: "1000px",
-                        padding: 50,
-                        borderRadius: 30,
-                        marginBottom: 50,
-                    }}
-                >
-                    <FormGroup row className="profile-imageContainer">
-                        <Label for="exampleFile" sm={2}>
-                            <div className="profile-imageLabel">IMAGE</div>
-                        </Label>
-                        <Avatar
-                            src="/broken-image.jpg"
-                            style={{ width: 150, height: 150, marginRight: 15 }}
-                        />
-                        <Col sm={6}></Col>
-                    </FormGroup>
+export default class MyProfile extends Component {
+    state = {
+        id: "",
+        email: "",
+        name: "",
+        gender: "",
+        age: "",
+    };
 
-                    <FormGroup row>
-                        <Label for="Id" sm={2} className="profile-margin">
-                            ID
-                        </Label>
-                        <Col sm={10}>
-                            <Input disabled type="id" name="id" id="Id" value={props.id} />
-                        </Col>
-                    </FormGroup>
+    requestInfo = async () => {
+        const token = await localStorage.getItem("token");
+        const bearer = `Bearer ${token}`;
 
-                    <FormGroup row style={{ marginBottom: 15 }}>
-                        <Label for="Name" sm={2} className="profile-margin">
-                            NAME
-                        </Label>
-                        <Col sm={10}>
-                            <Input disabled type="name" name="name" id="Name" value={props.name} />
-                        </Col>
-                    </FormGroup>
+        if (token == null) {
+            this.setState({
+                id: "비회원",
+                email: "비회원",
+                name: "비회원",
+            });
+            window.location.assign("/login");
+            alert("로그인이 필요한 페이지입니다. 로그인 후 이용해주세요.");
+        } else {
+            await axios
+                .post(URL.account, null, {
+                    headers: {
+                        Authorization: bearer,
+                    },
+                })
+                .then((res) => res.data)
+                .then((body) => {
+                    console.log(body);
 
-                    <FormGroup row>
-                        <Label for="Email" sm={2} className="profile-margin">
-                            EMAIL
-                        </Label>
-                        <Col sm={10}>
-                            <Input
-                                disabled
-                                type="email"
-                                name="email"
-                                id="Email"
-                                value={props.email}
+                    if (body.success) {
+                        const { user_id, email, name, gender, age } = body.data.user;
+                        this.setState({
+                            id: user_id,
+                            email,
+                            name,
+                            gender,
+                            age,
+                        });
+                    } else {
+                        alert(body.message);
+                    }
+                });
+        }
+    };
+
+    componentDidMount() {
+        this.requestInfo();
+    }
+
+    render() {
+        const { id, email, age, gender, name } = this.state;
+        return (
+            <div>
+                <h3 style={{ color: "gray", textShadow: "1px 1px 1px gray", marginBottom: 20 }}>
+                    나의 정보
+                </h3>
+                <Form className="profile-form">
+                    <Paper
+                        elevation={3}
+                        style={{
+                            width: "100%",
+                            minWidth: "1000px",
+                            padding: 50,
+                            borderRadius: 30,
+                            marginBottom: 50,
+                        }}
+                    >
+                        <FormGroup row className="profile-imageContainer">
+                            <Label for="exampleFile" sm={2}>
+                                <div className="profile-imageLabel">IMAGE</div>
+                            </Label>
+                            <Avatar
+                                src="/broken-image.jpg"
+                                style={{ width: 150, height: 150, marginRight: 15 }}
                             />
-                        </Col>
-                    </FormGroup>
+                            <Col sm={6}></Col>
+                        </FormGroup>
 
-                    <FormGroup row>
-                        <Label for="Password" sm={2} className="profile-margin">
-                            PASSWORD
-                        </Label>
-                        <Col sm={10}>
-                            <Input
-                                disabled
-                                type="password"
-                                name="password"
-                                id="Password"
-                                value="*************************"
-                                placeholder="현재 비밀번호를 입력해주세요."
-                            />
-                        </Col>
-                    </FormGroup>
+                        <FormGroup row>
+                            <Label for="Id" sm={2} className="profile-margin">
+                                ID
+                            </Label>
+                            <Col sm={10}>
+                                <Input disabled type="id" name="id" id="Id" value={id} />
+                            </Col>
+                        </FormGroup>
 
-                    <FormGroup row>
-                        <Label for="Gender" sm={2} className="profile-margin">
-                            Gender
-                        </Label>
-                        <Col sm={4}>
-                            <Input
-                                disabled
-                                type="text"
-                                name="gender"
-                                id="Gender"
-                                value={props.gender}
-                            />
-                        </Col>
-                        <Label for="Age" sm={1}>
-                            Age
-                        </Label>
-                        <Col sm={5}>
-                            <Input disabled type="age" name="age" id="Age" value={props.age} />
-                        </Col>
-                    </FormGroup>
-                </Paper>
-            </Form>
-        </div>
-    );
-};
+                        <FormGroup row style={{ marginBottom: 15 }}>
+                            <Label for="Name" sm={2} className="profile-margin">
+                                NAME
+                            </Label>
+                            <Col sm={10}>
+                                <Input disabled type="name" name="name" id="Name" value={name} />
+                            </Col>
+                        </FormGroup>
 
-export default MyProfile;
+                        <FormGroup row>
+                            <Label for="Email" sm={2} className="profile-margin">
+                                EMAIL
+                            </Label>
+                            <Col sm={10}>
+                                <Input
+                                    disabled
+                                    type="email"
+                                    name="email"
+                                    id="Email"
+                                    value={email}
+                                />
+                            </Col>
+                        </FormGroup>
+
+                        <FormGroup row>
+                            <Label for="Password" sm={2} className="profile-margin">
+                                PASSWORD
+                            </Label>
+                            <Col sm={10}>
+                                <Input
+                                    disabled
+                                    type="password"
+                                    name="password"
+                                    id="Password"
+                                    value="*************************"
+                                    placeholder="현재 비밀번호를 입력해주세요."
+                                />
+                            </Col>
+                        </FormGroup>
+
+                        <FormGroup row>
+                            <Label for="Gender" sm={2} className="profile-margin">
+                                Gender
+                            </Label>
+                            <Col sm={4}>
+                                <Input
+                                    disabled
+                                    type="text"
+                                    name="gender"
+                                    id="Gender"
+                                    value={gender}
+                                />
+                            </Col>
+                            <Label for="Age" sm={1}>
+                                Age
+                            </Label>
+                            <Col sm={5}>
+                                <Input disabled type="age" name="age" id="Age" value={age} />
+                            </Col>
+                        </FormGroup>
+                    </Paper>
+                </Form>
+            </div>
+        );
+    }
+}
