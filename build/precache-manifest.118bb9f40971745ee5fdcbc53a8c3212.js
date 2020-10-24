32a96ab00bbb7fb85c4c2b4dc3250c9a7ec74209
HeeYeonKim98 <wow9896@naver.com>
2020-10-25 04:57:26 +0900

    heroku 배포

---
+++ build/precache-manifest.118bb9f40971745ee5fdcbc53a8c3212.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298a0965cbadcea65b6d10596babbd04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a8c8eea45ca144c3595",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d1567424078813907827",
+    "url": "/static/css/main.12275186.chunk.css"
+  },
+  {
+    "revision": "8a8c8eea45ca144c3595",
+    "url": "/static/js/2.a337533f.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/static/js/2.a337533f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1567424078813907827",
+    "url": "/static/js/main.4ab507ef.chunk.js"
+  },
+  {
+    "revision": "8618e78bdabe368f0374",
+    "url": "/static/js/runtime-main.1ea6bfb0.js"
+  },
+  {
+    "revision": "2dbeae0ef2189bf8ddcef109e0508527",
+    "url": "/static/media/Logo.2dbeae0e.jpg"
+  },
+  {
+    "revision": "358f92963483337bb9994e1f4f13f647",
+    "url": "/static/media/Logo2.358f9296.png"
+  },
+  {
+    "revision": "09f46fa967e301a3f812b3adc18c38d0",
+    "url": "/static/media/Logo3.09f46fa9.png"
+  },
+  {
+    "revision": "d4918d573ef7141f66200ac7d980909a",
+    "url": "/static/media/Logo4.d4918d57.png"
+  },
+  {
+    "revision": "93a4b1246317ad1aaa2e0a64b3155d5f",
+    "url": "/static/media/couple_dinner.93a4b124.png"
+  },
+  {
+    "revision": "3c347f1fbdfdb9e58980fbc72f00fdb0",
+    "url": "/static/media/couplefeet.3c347f1f.jpg"
+  },
+  {
+    "revision": "8e3932f63c8067ea689fdc4682d6e7da",
+    "url": "/static/media/couplehands.8e3932f6.jpg"
+  },
+  {
+    "revision": "59c677247855d39e894b7536f7e3cca7",
+    "url": "/static/media/message_help.59c67724.gif"
+  },
+  {
+    "revision": "217ec85ba5f37bdbbb7ff53583c4603d",
+    "url": "/static/media/message_like.217ec85b.gif"
+  },
+  {
+    "revision": "b83dcec2fe04de0eed2172872ff97a39",
+    "url": "/static/media/message_mohe.b83dcec2.gif"
+  },
+  {
+    "revision": "f0ca871dd5ebbc0d2869d3e6bb3c5240",
+    "url": "/static/media/message_unlike.f0ca871d.gif"
+  },
+  {
+    "revision": "1043bf47dcac2a55c353778a6b9cc12b",
+    "url": "/static/media/pic1.1043bf47.jpg"
+  },
+  {
+    "revision": "3c0cbcdbc148a9dfce2b4c168abaff11",
+    "url": "/static/media/pic2.3c0cbcdb.jpg"
+  },
+  {
+    "revision": "f4444d31d3a851a61a69115ff410eea1",
+    "url": "/static/media/pic3.f4444d31.jpg"
+  },
+  {
+    "revision": "ac776816461076d9e9032f198097f761",
+    "url": "/static/media/wedding.ac776816.jpg"
+  },
+  {
+    "revision": "43f06b26c9912386de141bc6f65fd6cd",
+    "url": "/static/media/강민정.43f06b26.jpg"
+  },
+  {
+    "revision": "0d4245923d1f0417e4e6fd94eecf1f82",
+    "url": "/static/media/고은희.0d424592.jpg"
+  },
+  {
+    "revision": "d9c1f5f83c08c013f84f049432cb1fea",
+    "url": "/static/media/김희연.d9c1f5f8.jpg"
+  },
+  {
+    "revision": "92807711d1ef2458e0eb970e953edc95",
+    "url": "/static/media/설한정.92807711.jpg"
+  },
+  {
+    "revision": "69ca9acb11a9d136060279f904220f5c",
+    "url": "/static/media/이혜림.69ca9acb.jpg"
+  },
+  {
+    "revision": "d6f6f81917d993f5e432f05c0367ac8e",
+    "url": "/static/media/최지혜.d6f6f819.jpg"
+  }
+]);
